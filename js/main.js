d36d257aa84dbcba24e48a96d314fd36de755397
Xondamir Mo'minov <xondamir.muminov2008@gmail.com>
2021-03-25 10:55:13 +0500

    start spaces.com

--- js/main.js
+++ js/main.js
@@ -5,4 +5,55 @@ $(".home__slider").slick({
     speed:500,
     autoplay:true,
     arrows:true,
+    responsive: [
+        {
+          breakpoint: 1050,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+})
+$(".contact__slider").slick({
+    slidesToShow:3,
+    slidesToScroll:1,
+    speed:500,
+    autoplay:true,
+    arrows:true,
+    responsive: [
+        {
+          breakpoint: 950,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 800,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
 })
